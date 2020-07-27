6fcaf20f6b8a1b9245f248761967c0db2607ddab
Aman <amn.gusi@gmail.com>
2020-07-27 23:27:37 +0530

    Home and About Page

--- app.js
+++ app.js
@@ -45,3 +45,18 @@ loadPage = (page) => {
       $(`${page}`).addClass("slide-in-bck-center");
    }, 2000);
 };
+$(function () {
+   var btn = $(".btn");
+   btn.on("click", function () {
+      $(this).addClass("btn__progress");
+      setTimeout(function () {
+         btn.addClass("btn__progress--fill");
+      }, 500);
+      setTimeout(function () {
+         btn.removeClass("btn__progress--fill");
+      }, 4100);
+      setTimeout(function () {
+         btn.addClass("btn__complete");
+      }, 4400);
+   });
+});
