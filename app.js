0836570ee876563b90864c4c8d130605a3056a8d
Aman <amn.gusi@gmail.com>
2020-08-11 20:33:45 +0530

    rubberband animation

--- app.js
+++ app.js
@@ -67,3 +67,18 @@ clearInput = () => {
    $("#subject").val(" ");
    $("#message").val(" ");
 };
+
+var square = $(".a");
+
+for (var i = 0; i < square.length; i++) {
+   square[i].addEventListener("mouseenter", function () {
+      this.classList.add("rubberBand");
+      this.addEventListener(
+         "animationend",
+         function () {
+            this.classList.remove("rubberBand");
+         },
+         false
+      );
+   });
+}
